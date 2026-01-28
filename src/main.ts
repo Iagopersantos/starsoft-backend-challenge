e653f51e8f0e2f917d90726c38695937d4f85e7d
iago <iago.pereira@nectarcrm.com.br>
2026-01-28 12:37:57 -0300

    adjust infra

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3333);
+  await app.listen(Number(process.env.PORT ?? 3000), '0.0.0.0');
 }
 bootstrap();
